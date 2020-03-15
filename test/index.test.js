abf5bfdf89bdf9d05658bc41c88569ec4a1f25d9
Jim Yiu <1195022367@qq.com>
2020-03-15 15:43:33 +0800

    增加reademe文件

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@
 require('../dist/index');
 const expect = require('chai').expect;
 
-describe('测试 获取每周周一', () => {
+describe('测试 获取每周周一 ', () => {
   it('should return 2', () => {
     const result = new Date().getCurrWeekFirst().getDay();
     expect(result).to.equal(1);
